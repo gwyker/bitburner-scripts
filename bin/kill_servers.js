afa599fbdc69efe3371e84c8d16072135b932452
Grayson Wyker <gwyker@yahoo.com>
2022-02-20 15:41:30 -0500

    move utils to bin

--- bin/kill_servers.js
+++ bin/kill_servers.js
@@ -1,14 +1,21 @@
-import { getServers } from "/lib/server.js"
+import { killServer, getServers } from "/lib/server.js"
 
 /** @param {NS} ns **/
+/** @param {import(".").NS } ns */
 export async function main(ns) {
+    killServers(ns, ns.args[0])
+}
+
+export function killServers(ns, threshold) {
+    if (!threshold) {
+        ns.alert('Give a maximum threshold gb for server-killing.')
+        return
+    }
     let servers = getServers(ns)
     for (const s of servers) {
-        if (!s.includes('goop')) {
-            continue
+        if (ns.getServerMaxRam(s) <= threshold && s.includes('goop')) {
+            ns.tprint(`Killing ${s}...`)
+            killServer(ns, s)
         }
-        ns.killall(s)
-        ns.deleteServer(s)
-        ns.tprint('Deleted '+s)
     }
 }
